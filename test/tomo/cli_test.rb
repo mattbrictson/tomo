be611e1d3137f7906ca623d59f19ba0a86d2aa4a
Matt Brictson <matt@mattbrictson.com>
2019-07-13 13:08:56 -0700

    cleanup

--- test/tomo/cli_test.rb
+++ test/tomo/cli_test.rb
@@ -6,18 +6,16 @@ class Tomo::CLITest < Minitest::Test
   def test_execute_task_with_implicit_run_command
     in_temp_dir do
       tomo "init"
-      tomo "bundler:install", "--dry-run"
+      stdout = tomo "bundler:install", "--dry-run"
       assert_match "Simulated bundler:install", stdout
     end
   end
 
   private
 
-  attr_reader :stdout
-
   def tomo(*args)
     with_tomo_gemfile do
-      @stdout = capture("bundle", "exec", "tomo", *args.flatten)
+      capture("bundle", "exec", "tomo", *args.flatten)
     end
   end
 end
