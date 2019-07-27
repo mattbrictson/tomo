8a770f54effc3b7827b73282a9472954cd600589
Matt Brictson <matt@mattbrictson.com>
2019-07-26 20:49:44 -0700

    Allow bundler version to specified via setting
    Before, the `bundler:upgrade_bundler` task would work by checking the
    `Gemfile.lock` to determine the version of bundler to install. Sometimes
    we want to specify the version explicitly instead of relying on this
    magic.
    
    In an earlier commit, support for manually specifying a bundler version
    was done via a command-line argument, but this is not consistent with
    other tomo plugins. Other plugins like nvm and rbenv have a setting that
    controls the version. The nice thing about this is settings also work on
    the command line via the `-s` option, so it is a nice solution that
    supports both styles.
    
    This commit adds a `:bundler_version` setting for this purpose. If the
    setting is `nil` (the default), it falls back to the auto-detect
    behavior that existed previously. For consistency, the `:run_args`
    solution introduced in the earlier commit has been removed.

--- test/tomo/plugin/bundler/tasks_test.rb
+++ test/tomo/plugin/bundler/tasks_test.rb
@@ -2,12 +2,9 @@ require "test_helper"
 require "tomo/plugin/bundler"
 
 class Tomo::Plugin::Bundler::TasksTest < Minitest::Test
-  def setup
-    @tester = Tomo::Testing::MockPluginTester.new("bundler")
-  end
-
-  def test_upgrade_bundler
-    @tester.mock_script_result(/^tail .*Gemfile\.lock/, stdout: <<~OUT)
+  def test_upgrade_bundler_uses_lock_file_for_version
+    tester = configure
+    tester.mock_script_result(/^tail .*Gemfile\.lock/, stdout: <<~OUT)
         minitest-ci (~> 3.4)
         minitest-hooks (~> 1.5)
         minitest-reporters (~> 1.3)
@@ -19,24 +16,32 @@ class Tomo::Plugin::Bundler::TasksTest < Minitest::Test
       BUNDLED WITH
          2.0.2
     OUT
-    @tester.run_task("bundler:upgrade_bundler")
+    tester.run_task("bundler:upgrade_bundler")
     assert_equal(
       "gem install bundler --conservative --no-document -v 2.0.2",
-      @tester.executed_scripts.last
+      tester.executed_scripts.last
+    )
+  end
+
+  def test_upgrade_bundler_uses_setting_for_version
+    tester = configure(bundler_version: "2.0.1")
+    tester.run_task("bundler:upgrade_bundler")
+    assert_equal(
+      "gem install bundler --conservative --no-document -v 2.0.1",
+      tester.executed_script
     )
   end
 
   def test_upgrade_bundler_skips_installation_if_lock_file_is_absent
-    @tester.mock_script_result(/^tail .*Gemfile\.lock/, exit_status: 1)
-    @tester.run_task("bundler:upgrade_bundler")
-    assert_match(/tail .*Gemfile\.lock/, @tester.executed_script)
+    tester = configure
+    tester.mock_script_result(/^tail .*Gemfile\.lock/, exit_status: 1)
+    tester.run_task("bundler:upgrade_bundler")
+    assert_match(/tail .*Gemfile\.lock/, tester.executed_script)
   end
 
-  def test_upgrade_bundler_with_explicit_version_arg
-    @tester.run_task("bundler:upgrade_bundler", "3.0.0")
-    assert_equal(
-      "gem install bundler --conservative --no-document -v 3.0.0",
-      @tester.executed_script
-    )
+  private
+
+  def configure(settings={})
+    Tomo::Testing::MockPluginTester.new("bundler", settings: settings)
   end
 end
