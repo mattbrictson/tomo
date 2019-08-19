6137a80ac20ed7762e7120e8c665b2f8f54a27a7
Matt Brictson <mattbrictson@users.noreply.github.com>
2019-08-18 22:10:25 -0700

    Fix hang when starting puma 4.1.0 (#39)
    Puma 4.1.0 no longer flushes stdout before daemonizing. This causes tomo
    to hang indefinitely after calling `puma --daemon` because the SSH
    connection does not cleanly exit.
    
    Fix by explicitly redirecting stdout and stderr to files. This has an
    added benefit of storing troubleshooting information in cases where puma
    fails to start for other reasons.

--- lib/tomo/plugin/puma/tasks.rb
+++ lib/tomo/plugin/puma/tasks.rb
@@ -3,10 +3,8 @@ module Tomo::Plugin::Puma
     def restart
       return if try_restart
 
-      remote.chdir(paths.current) do
-        logger.info "Puma is not running. Starting it now."
-        remote.bundle("exec", "puma", "--daemon", *control_options)
-      end
+      logger.info "Puma is not running. Starting it now."
+      start
     end
 
     private
@@ -24,6 +22,25 @@ module Tomo::Plugin::Puma
 
       logger.info(ctl_result.output)
       true
+    end
+
+    def start
+      require_settings :puma_stdout_path, :puma_stderr_path
+
+      ensure_output_directory
+
+      remote.chdir(paths.current) do
+        remote.bundle(
+          "exec", "puma", "--daemon", *control_options,
+          raw(">"), paths.puma_stdout,
+          raw("2>"), paths.puma_stderr
+        )
+      end
+    end
+
+    def ensure_output_directory
+      dirs = [paths.puma_stdout, paths.puma_stderr].map(&:dirname).map(&:to_s)
+      remote.mkdir_p dirs.uniq
     end
 
     def control_options
