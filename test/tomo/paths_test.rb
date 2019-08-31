c385a370bcbf4e302bf78bd49fa45c2a36f1261e
Matt Brictson <matt@mattbrictson.com>
2019-08-31 13:05:42 -0700

    Handle nil path settings more consistently
    Before, if a path setting was `nil`, accessing that setting via the
    `paths` DSL would return an empty string, which is inconsistent. Now the
    DSL will also return `nil` in that scenario.

---
+++ test/tomo/paths_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class Tomo::PathsTest < Minitest::Test
+  def test_raises_if_setting_does_not_exist
+    paths = Tomo::Paths.new({})
+    assert_raises(NoMethodError) { paths.storage }
+  end
+
+  def test_returns_nil_if_setting_is_nil
+    paths = Tomo::Paths.new(storage_path: nil)
+    assert_nil(paths.storage)
+  end
+end
