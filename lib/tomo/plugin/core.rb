6418e0e5969f5b6d2cdef63c51d2acbc1e6b5f0d
Matt Brictson <matt@mattbrictson.com>
2019-09-17 12:37:40 -0700

    Add :tomo_config_file_path setting
    Automatically set based on where the configuration is loaded from.

--- lib/tomo/plugin/core.rb
+++ lib/tomo/plugin/core.rb
@@ -9,20 +9,21 @@ module Tomo::Plugin
     tasks Tomo::Plugin::Core::Tasks
 
     defaults Tomo::SSH::Options::DEFAULTS.merge(
-      application:       "default",
-      concurrency:       10,
-      current_path:      "%<deploy_to>/current",
-      deploy_to:         "/var/www/%<application>",
-      keep_releases:     10,
-      linked_dirs:       [],
-      linked_files:      [],
-      local_user:        nil, # determined at runtime
-      release_json_path: "%<release_path>/.tomo_release.json",
-      releases_path:     "%<deploy_to>/releases",
-      revision_log_path: "%<deploy_to>/revisions.log",
-      shared_path:       "%<deploy_to>/shared",
-      tmp_path:          "/tmp/tomo",
-      run_args:          [] # determined at runtime
+      application:           "default",
+      concurrency:           10,
+      current_path:          "%<deploy_to>/current",
+      deploy_to:             "/var/www/%<application>",
+      keep_releases:         10,
+      linked_dirs:           [],
+      linked_files:          [],
+      local_user:            nil, # determined at runtime
+      release_json_path:     "%<release_path>/.tomo_release.json",
+      releases_path:         "%<deploy_to>/releases",
+      revision_log_path:     "%<deploy_to>/revisions.log",
+      shared_path:           "%<deploy_to>/shared",
+      tmp_path:              "/tmp/tomo",
+      tomo_config_file_path: nil, # determined at runtime
+      run_args:              [] # determined at runtime
     )
   end
 end
