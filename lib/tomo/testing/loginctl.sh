1676512adbee63a254951d834b098cdb5b905bfd
Matt Brictson <matt@mattbrictson.com>
2019-10-19 13:48:56 -0700

    Explain loginctl stub

--- lib/tomo/testing/loginctl.sh
+++ lib/tomo/testing/loginctl.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+# THIS FILE IS FOR TESTING PURPOSES ONLY.
+#
+# The real loginctl command does not work in a Docker container, so this empty
+# script takes its place, allowing tomo to work during an E2E test.
