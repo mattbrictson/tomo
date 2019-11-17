1f1605fb3a216f691f87d5c0034ddb1dc01e4a34
Matt Brictson <mattbrictson@users.noreply.github.com>
2019-11-16 18:03:11 -0800

    Show friendly error when plugin is missing (#77)
    If you try to run a task but the namespace of the task is not
    recognized, tomo will now helpfully suggest that you may need to load
    the plugin that defines that namespace. E.g.
    
    ```
    $ tomo run sidekiq:setup
    tomo run v0.12.0
    
      ERROR: sidekiq:setup is not a recognized task.
      To see a list of all available tasks, run tomo tasks.
    
      Did you forget to install the sidekiq plugin?
    ```

--- lib/tomo/runtime/unknown_task_error.rb
+++ lib/tomo/runtime/unknown_task_error.rb
@@ -9,14 +9,27 @@ module Tomo
           To see a list of all available tasks, run #{blue('tomo tasks')}.
         ERROR
 
-        # TODO: suggest "did you forget to add the <abc> plugin?"
+        sugg = spelling_suggestion || missing_plugin_suggestion
+        error << sugg if sugg
+        error
+      end
 
+      private
+
+      def spelling_suggestion
         sugg = Error::Suggestions.new(
           dictionary: known_tasks,
           word: unknown_task
         )
-        error << sugg.to_console if sugg.any?
-        error
+        sugg.to_console if sugg.any?
+      end
+
+      def missing_plugin_suggestion
+        unknown_plugin = unknown_task[/\A(.+?):/, 1]
+        known_plugins = known_tasks.map { |t| t.split(":").first }.uniq
+        return if unknown_plugin.nil? || known_plugins.include?(unknown_plugin)
+
+        "\nDid you forget to install the #{blue(unknown_plugin)} plugin?"
       end
     end
   end
