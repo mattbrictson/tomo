287451ca8388cb6d8fff3a7a40351d9fd964f297
Matt Brictson <matt@mattbrictson.com>
2019-12-28 17:25:43 -0600

    Fix RuboCop 0.78.0 issues

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,4 @@ require "tomo/testing"
 
 require "minitest/autorun"
 require "minitest/hooks/test"
-Dir[File.expand_path("support/**/*.rb", __dir__)].each { |rb| require(rb) }
+Dir[File.expand_path("support/**/*.rb", __dir__)].sort.each { |rb| require(rb) }
