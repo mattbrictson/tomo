b4d66e006ec342bc9d0df094d5c4e6443c5c1075
Matt Brictson <matt@mattbrictson.com>
2020-05-12 22:24:44 -0700

    Use rake to invoke rake tasks, not rails
    The documentation for tomo's `rake` helper, and the intent of that
    method, is that it runs `bundle exec rake`. However this `rake` helper
    was in fact running `bundle exec rails` instead. This was a mistake.
    
    Fix this so that the `rake` helper, and tasks that depend on it (e.g.
    `rails:db_migrate`, `rails:db_seed`, etc.) use `bundle exec rake`
    instead of `bundle exec rails` as originally intended.
    
    This also makes tomo compatible with older versions of Rails, before the
    `rails` was smart enough to be able to invoke rake tasks.

---
+++ test/tomo/plugin/rails/helpers_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+require "tomo/plugin/rails"
+
+class Tomo::Plugin::Rails::HelpersTest < Minitest::Test
+  def test_rake_runs_bundle_exec_rake_in_current_path
+    tester = Tomo::Testing::MockPluginTester.new(
+      "bundler", "rails", settings: { current_path: "/app/current" }
+    )
+    tester.call_helper(:rake, "db:migrate")
+    assert_equal(
+      "cd /app/current && bundle exec rake db:migrate",
+      tester.executed_script
+    )
+  end
+end
