97c5a86ad68fc04d8d40fef6ce3a0ed3ec06ac55
Matt Brictson <matt@mattbrictson.com>
2022-08-30 21:27:40 -0700

    Add helper for running thor tasks with remote.thor

--- test/tomo/plugin/rails/helpers_test.rb
+++ test/tomo/plugin/rails/helpers_test.rb
@@ -7,4 +7,10 @@ class Tomo::Plugin::Rails::HelpersTest < Minitest::Test
     tester.call_helper(:rake, "db:migrate")
     assert_equal("cd /app/current && bundle exec rake db:migrate", tester.executed_script)
   end
+
+  def test_thor_runs_bundle_exec_thor_in_current_path
+    tester = Tomo::Testing::MockPluginTester.new("bundler", "rails", settings: { current_path: "/app/current" })
+    tester.call_helper(:thor, "user:create")
+    assert_equal("cd /app/current && bundle exec thor user:create", tester.executed_script)
+  end
 end
